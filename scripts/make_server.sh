8667f884bf226260f7571c290cd560c13052d546
Eclixal <28450187+eclixal@users.noreply.github.com>
2022-07-05 21:14:08 +0200

    update to 1.19

--- scripts/make_server.sh
+++ scripts/make_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --output BuildTools.jar
-java -Xmx6G -Xms6G -XX:MaxDirectMemorySize=256M -jar BuildTools.jar --rev 1.18.2
+java -Xmx6G -Xms6G -XX:MaxDirectMemorySize=256M -jar BuildTools.jar --rev 1.19
 mv spigot-*.jar straw-serv.jar
